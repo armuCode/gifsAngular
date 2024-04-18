994c94b81289d979c3ddeb235a52f5ddd2e0f9d5
armucode <armucode@gmail.com>
2024-04-17 20:15:43 -0500

    Se grega la page del gif

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,15 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomeComponent } from './pages/home/home.component';
-
-
+import { HomePageComponent } from './pages/home/home-page.component';
 
 @NgModule({
-  declarations: [
-    HomeComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HomePageComponent],
+  imports: [CommonModule],
+  exports: [HomePageComponent],
 })
-export class GifsModule { }
+export class GifsModule {}
