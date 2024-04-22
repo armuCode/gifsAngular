018a988a6aef76d9f5f5ed8d66cab53be4b98c99
armucode <armucode@gmail.com>
2024-04-22 14:54:52 -0500

    Se crea el service fetch para el llamado de los gifs

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,10 +1,13 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class GifsService {
   private _tagsHistory: string[] = [];
+  private apiKey: string = 'DUlXd2EBD0DG424cX84FX22DKJpEJXmt';
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   get tagsHistory() {
     return [...this._tagsHistory];
@@ -24,6 +27,23 @@ export class GifsService {
   searchTag(tag: string): void {
     if (tag.length === 0) return;
     this.organizeHistory(tag);
+
+    // await fetch(
+    //   'https://api.giphy.com/v1/gifs/search?api_key=DUlXd2EBD0DG424cX84FX22DKJpEJXmt&q=valorant&limit=10'
+    // ).then((response) => {
+    //   response.json().then((data) => {
+    //     console.log(data);
+    //   });
+    // });
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', '10')
+      .set('q', tag);
+
+    this.http
+      .get(`${this.serviceUrl}/search`, { params })
+      .subscribe((resp) => console.log(resp));
 
     console.log(this.tagsHistory);
   }
